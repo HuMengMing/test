773f6bd72511c4879b442faa9184a06372b41d13
HUMENGMING <humengmingxx@gmail.com>
2018-12-10 22:44:23 +0800

    第一次尝试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    title: '测试标题', // 设置网站标题
+    description: '测试描述', //描述
+    dest: './dist', // 设置输出目录
+    port: 2233, //端口
+    themeConfig: { //主题配置
+        // 添加导航栏
+        nav: [{
+                text: '测试主页',
+                link: '/'
+            }, // 导航条
+            {
+                text: '测试文档',
+                link: '/testTemp/'
+            }
+            // {
+            //     text: 'github', // 这里是下拉列表展现形式。
+            //     items: [{
+            //             text: 'focus-outside',
+            //             link: 'https://github.com/TaoXuSheng/focus-outside'
+            //         },
+            //         {
+            //             text: 'stylus-converter',
+            //             link: 'https://github.com/TaoXuSheng/stylus-converter'
+            //         },
+            //     ]
+            // }
+        ],
+        // 为以下路由添加侧边栏
+        sidebar: {
+            '/testTemp/': [
+                {
+                    title: '测试组件',
+                    collapsable: true,
+                    children: [
+                        'base/test'
+                    ]
+                }
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 你好，我是测试1
+  details: 测试1
+- title: 你好，我是测试2
+  details: 测试2
+- title: 你好，我是测试3
+  details: 测试3
+footer: 测试测试测试
+---
